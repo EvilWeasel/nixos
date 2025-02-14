12c7f22f5de1bc9e2580e685e6d2d3c80e2fceac
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2025-02-14 16:57:23 +0100

    wifi fix

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -20,6 +20,7 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # somehow sometimes need to restart `iwd` service???
   networking = {
     networkmanager = {
       enable = lib.mkDefault true;
